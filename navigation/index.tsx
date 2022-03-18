1de41e76547e79eaae970b5dd9027ecc22a91db0
M0ngi <saidanemongi@gmail.com>
2022-03-18 15:27:56 +0100

    Add basic navigation

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,15 +1,29 @@
-import React, { useContext } from 'react';
+import { User } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { auth } from '../config';
 import { InfoContext } from '../contexts/InfoProvider';
+import { HomeScreen } from '../screens/home_screen/HomeScreen';
 import LoadingScreen from '../screens/loading_screen/LoadingScreen';
 import { LoginPageScreen } from '../screens/login_screen/LoginPageScreen';
 
+function mainScreen(user : User | null) : JSX.Element{
+    if(!user) return <LoginPageScreen />;
+
+    return <HomeScreen />;
+}
+
 export function Navigator() : JSX.Element{
     const info = useContext(InfoContext);
+    const [user, setUser] = useState(auth.currentUser);
+
+    auth.onAuthStateChanged((currUser) => {
+        setUser(currUser);
+    })
 
     return (
         <>
             {
-                info.loading ? <LoadingScreen /> : <LoginPageScreen />
+                info.loading ? <LoadingScreen /> : mainScreen(user)
             }
         </>
     );
---
+++ screens/home_screen/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React, { useContext, useState } from "react";
+
+export function HomeScreen() : JSX.Element{
+    return (
+        <>
+        </>
+    );
+}
